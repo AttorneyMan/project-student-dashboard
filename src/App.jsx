e290c70b18fae2fa2f58f5fbc10722834cf45227
Anthony O <antman@ANTMANS-Air.lan>
2023-08-08 10:06:41 -0400

    still messing with compentents and cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
+import data from "./data/data.json";
+
 
 function App() {
-  return (
-    <div>
+
+  const studentsForRender = data.map(
+    
+    (eachStudentObj)=>{
+        return(<h4>{eachStudentObj.username}</h4>)
+       }
+   
+)
+   return (
+    <div class="list">
+      
       <h1>Student Dashboard</h1>
+       {studentsForRender}
     </div>
-  );
-}
+    );
+  }
+     
+  
+  
+  
+   
+ 
 
 export default App;
---
+++ src/components/studentList.jsx
@@ -0,0 +1,30 @@
+// import data from "../data/data.json"
+// // import { useState } from "react";
+
+//  function studentList(){
+//    const studentsForRender = data.map(
+    
+//     (eachStudentObj)=>{
+//         return( <div>
+//        <img src="" alt= "Image of student" />
+//        <h4>{eachStudentObj.username}</h4>
+
+//         </div>
+        
+//         )
+//        }
+   
+// )
+//    return(
+// <div class="list">
+// <h4>List Of Students</h4>
+// {studentList}
+
+// </div>
+
+
+//    );
+
+// }
+
+// export default  studentList();
